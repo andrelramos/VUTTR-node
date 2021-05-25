7a83d11c105834d648e1c96cf628705e4e44f793
André Ramos <andrel.ramos97@gmail.com>
2021-05-24 22:55:20 -0300

    Lets APIs throw unhandled exceptions to get the results on an APM tool

--- src/tools/routes.js
+++ src/tools/routes.js
@@ -5,30 +5,18 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', async (req, res) => {
-  try {
-    const result = await controller.getTools(req.query.tag);
-    res.json(result);
-  } catch {
-    res.status(404);
-  }
+  const result = await controller.getTools(req.query.tag);
+  res.json(result);
 });
 
 router.post('/', async (req, res) => {
-  try {
-    const result = await controller.saveTool(req.body);
-    res.json(result);
-  } catch {
-    res.status(404);
-  }
+  const result = await controller.saveTool(req.body);
+  res.json(result);
 });
 
 router.delete('/:id', async (req, res) => {
-  try {
-    const result = await controller.deleteTool(req.params.id);
-    res.json(result);
-  } catch {
-    res.status(404);
-  }
+  const result = await controller.deleteTool(req.params.id);
+  res.json(result);
 });
 
 module.exports = router;
