06e1f1a293daf30999e8a3041a44d90f8bf329a3
André Ramos <andrel.ramos97@gmail.com>
2021-05-24 22:54:30 -0300

    Adding dotenv

--- app.js
+++ app.js
@@ -4,17 +4,26 @@ const logger = require('morgan')
 const swaggerUi = require('swagger-ui-express')
 const toolsRouter = require('./src/tools/routes')
 const swaggerDocument = require('./swagger.json')
+const dotenv = require('dotenv')
 
+// Application settings
 const app = express()
 const port = 3000
+dotenv.config()
 
+// Configs middlewares
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+// Application routes
 app.use('/tools', toolsRouter)
 
+// Connection with database
 mongoose.connect(process.env.MONGO_URI,  {useNewUrlParser: true})
 
+// Starting express server
 app.listen(port, () => console.log(`Running server on port ${port}`))
