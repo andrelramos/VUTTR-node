3c4c0b3de725e6ff3d39f6fb473b04ecdeee4ba3
André Ramos <andrel.ramos97@gmail.com>
2021-05-24 23:00:12 -0300

    Using .env on test sandbox

--- run-tests-sandbox.sh
+++ run-tests-sandbox.sh
@@ -6,9 +6,6 @@
 #                                               #           
 #################################################
 
-# SETTING TEST ENVS
- export MONGO_URI=mongodb://mongo:27017/vuttrtest
-
 # RUN MONGO FOR TEST
 docker run -it --rm --name=mongo_test -p 27017:27017 --tmpfs /data/db mongo:3.6 --storageEngine ephemeralForTest --nojournal --logpath /dev/null &
 
