409d0dc827b3a1a96e2bd70c5f59d8154dfecaa2
André Ramos <andrel.ramos97@gmail.com>
2021-05-24 23:29:07 -0300

    Adding semicolons

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose')
-const express = require('express')
-const logger = require('morgan')
-const swaggerUi = require('swagger-ui-express')
-const toolsRouter = require('./src/tools/routes')
-const swaggerDocument = require('./swagger.json')
-const dotenv = require('dotenv')
+const mongoose = require('mongoose');
+const express = require('express');
+const logger = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const dotenv = require('dotenv');
+const toolsRouter = require('./src/tools/routes');
+const swaggerDocument = require('./swagger.json');
 
 // Application settings
-const app = express()
-const port = 3000
-dotenv.config()
+const app = express();
+const port = 3000;
+dotenv.config();
 
 // Configs middlewares
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Application routes
-app.use('/tools', toolsRouter)
+app.use('/tools', toolsRouter);
 
 // Connection with database
-mongoose.connect(process.env.MONGO_URI,  {useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 // Starting express server
-app.listen(port, () => console.log(`Running server on port ${port}`))
+app.listen(port);
