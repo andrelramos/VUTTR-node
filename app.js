172b7fd2f88134c5c6deff6fb9f752f1c8e6a10c
André Ramos <andrel.ramos97@gmail.com>
2021-05-24 22:40:35 -0300

    Adding app listen

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-var mongoose = require('mongoose')
-var express = require('express')
-var logger = require('morgan')
-var swaggerUi = require('swagger-ui-express')
-var toolsRouter = require('./src/tools/routes')
-var swaggerDocument = require('./swagger.json')
+const mongoose = require('mongoose')
+const express = require('express')
+const logger = require('morgan')
+const swaggerUi = require('swagger-ui-express')
+const toolsRouter = require('./src/tools/routes')
+const swaggerDocument = require('./swagger.json')
 
-var app = express()
+const app = express()
+const port = 3000
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-
 app.use('/tools', toolsRouter)
 
 mongoose.connect(process.env.MONGO_URI,  {useNewUrlParser: true})
 
-module.exports = app
+app.listen(port, () => console.log(`Running server on port ${port}`))
